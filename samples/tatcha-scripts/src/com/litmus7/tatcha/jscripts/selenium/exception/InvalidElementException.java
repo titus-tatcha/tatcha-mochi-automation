a0a62cbb390c5db6091067b74933a4cf342fde3d
Titus <titus@tatcha.com>
2018-01-29 16:36:44 +0530

    missing files commit

--- samples/tatcha-scripts/src/com/litmus7/tatcha/jscripts/selenium/exception/InvalidElementException.java
+++
@@ -1,7 +0,0 @@
-package com.litmus7.tatcha.jscripts.selenium.exception;
-
-public class InvalidElementException extends Exception {
-	public InvalidElementException(String exception){
-		super(exception);
-	}
-}
--- samples/tatcha-scripts/src/com/litmus7/tatcha/jscripts/selenium/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.litmus7.tatcha.jscripts.selenium.exception;
-
-public class ProductNotFoundException extends Exception {
-	public ProductNotFoundException(String exception){
-		super(exception);
-	}
-}
