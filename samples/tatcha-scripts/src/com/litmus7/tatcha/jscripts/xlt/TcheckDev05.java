b43951c3504fd36ce58b901e25b376d99798d195
Titus <titus@tatcha.com>
2018-01-29 16:40:45 +0530

    missing files

--- samples/tatcha-scripts/src/com/litmus7/tatcha/jscripts/xlt/TcheckDev05.java
+++ samples/tatcha-scripts/src/com/litmus7/tatcha/jscripts/xlt/TcheckDev05.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import com.litmus7.tatcha.utils.BrowserDriver;
+
 import com.xceptance.xlt.api.engine.scripting.AbstractWebDriverScriptTestCase;
 
 /**
